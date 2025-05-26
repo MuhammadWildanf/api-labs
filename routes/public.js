0f4e684bdb0dece5ec07aa13ae0ed259580e5f96
Muhammad Wildan Ferdiansyah <92373802+MuhammadWildanf@users.noreply.github.com>
2025-05-26 21:08:54 +0700

    fix route

--- routes/public.js
+++ routes/public.js
@@ -3,7 +3,7 @@ const Public = require("../controllers/public");
 const public = express.Router();
 
 public.get("/products", Public.product);
-public.get("/products/:id", Public.productDetail);
+// public.get("/products/:id", Public.productDetail);
 public.get("/products/slug/:slug", Public.showBySlug);
 public.get("/posts", Public.post);
 public.get("/posts/:id", Public.postDetail);
